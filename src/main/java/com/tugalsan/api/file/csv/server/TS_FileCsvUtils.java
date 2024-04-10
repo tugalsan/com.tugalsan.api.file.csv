258f59f1bd86a7adcea401a484de9b3b89cba4de
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-10 05:01:16 +0300

    Update TS_FileCsvUtils.java

--- src/main/java/com/tugalsan/api/file/csv/server/TS_FileCsvUtils.java
+++ src/main/java/com/tugalsan/api/file/csv/server/TS_FileCsvUtils.java
@@ -6,27 +6,28 @@ import java.util.*;
 import java.util.stream.*;
 import com.tugalsan.api.list.client.*;
 import com.tugalsan.api.union.client.TGS_Union;
+import com.tugalsan.api.union.client.TGS_UnionExcuse;
 import java.io.IOException;
 
 public class TS_FileCsvUtils {
 
-    public static TGS_Union<List<TGS_Union<Boolean>>> toFile(TGS_ListTable source, Path destFile, boolean excelStyle) {
-        List<TGS_Union<Boolean>> results = TGS_ListUtils.of();
+    public static TGS_Union<List<TGS_UnionExcuse>> toFile(TGS_ListTable source, Path destFile, boolean excelStyle) {
+        List<TGS_UnionExcuse> results = TGS_ListUtils.of();
         try (var writer = Files.newBufferedWriter(destFile);) {
             var f = excelStyle ? CSVFormat.EXCEL.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim() : CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim();
             var csvPrinter = new CSVPrinter(writer, f);
             source.getRows().stream()
                     .map(row -> row == null ? TGS_ListUtils.of() : (List) row)
                     .map(row -> {
-                        TGS_Union<Boolean> u;
+                        TGS_UnionExcuse u;
                         try {
                             csvPrinter.printRecord(row);
-                            u = TGS_Union.of(true);
+                            u = TGS_UnionExcuse.ofVoid();
                         } catch (IOException ex) {
-                            u = TGS_Union.ofExcuse(ex);
+                            u = TGS_UnionExcuse.ofExcuse(ex);
                         }
                         return u;
-                    }).forEachOrdered(u -> results.add((TGS_Union<Boolean>) u));
+                    }).forEachOrdered(u -> results.add((TGS_UnionExcuse) u));
             csvPrinter.flush();
             return TGS_Union.of(results);
         } catch (IOException ex) {
